6716f83054e53c8db08c229d4e3d27e493b18b6b
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-01 12:12:49 +0200

    Init models (#1)
    * Add serde
    
    * Add JWT model

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![forbid(unsafe_code)]
+#![deny(
+    dead_code,
+    unused_imports,
+    unused_mut,
+    missing_docs,
+    missing_debug_implementations
+)]
+//! Manage communication between Turms and client.
+
+pub mod jwt;
