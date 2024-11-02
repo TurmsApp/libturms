37b1f768ade40cd7c85719ce5239bf83fae74e17
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-11-02 16:29:37 +0100

    Remove unused function

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,17 +1,9 @@
 //! Models for WebSocket messages and Turms structures.
 
-pub mod response;
 pub mod phoenix;
+pub mod response;
 
 use serde::de::Deserialize;
-
-/// Convert [`u64`] into [`String`].
-pub(crate) fn u64_to_string<S>(num: &u64, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&num.to_string())
-}
 
 /// Convert a [`String`] into [`u64`].
 pub(crate) fn string_to_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
