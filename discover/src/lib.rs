866109601ec294c5d9887a082eef94ad308ba974
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-06-14 20:36:41 +0200

    Rework library (#9)
    * Better structure
    
    * Add heartbeat sender

--- discover/src/lib.rs
+++ discover/src/lib.rs
@@ -1,15 +1,15 @@
+//! High level API to communicate with discovery Turms service.
 #![forbid(unsafe_code)]
-/*#![deny(
+#![deny(
     dead_code,
     unused_imports,
     unused_mut,
     missing_docs,
     missing_debug_implementations
-)]*/
-//! Manage communication between Turms and client.
+)]
 
-pub mod error;
-mod future;
 pub mod jwt;
 pub mod models;
 pub mod websocket;
+#[macro_use]
+mod macros;
