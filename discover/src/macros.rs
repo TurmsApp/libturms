ccbf45aca0551148eb6d5746a9ace5da61b2f8a3
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-06-16 18:45:15 +0200

    Remove Tokio select

--- discover/src/macros.rs
+++ discover/src/macros.rs
@@ -3,34 +3,29 @@
 /// Spawn heartbeat to keep WebSocket connection active.
 #[macro_export]
 macro_rules! spawn_heartbeat {
-    (mut $ws:expr) => {{
-        if let Some(client) = $ws.client.as_ref() {
+    ($ws:expr) => {{
+        let mut client = $ws.client.clone();
             use tokio::time::interval;
             use futures_util::sink::SinkExt;
             use $crate::models::phoenix::Message;
 
-            // Initialize the interval timer for sending heartbeat messages
+            // Initialize the interval timer for sending heartbeat messages.
             let mut heartbeat_interval = interval($ws.heartbeat_delay);
 
             tokio::spawn(async move {
                 loop {
-                    tokio::select! {
-                        // Heartbeat handler to send periodic messages
-                        _ = heartbeat_interval.tick() => {
-                            // Send heartbeat message.
-                            // If another thread use process, don't worry because this process will do ping
-                            // for us!
-                            let msg: Message<String> = Message::default().event(models::phoenix::Event::Heartbeat);
-                            match $ws.send(msg).await {
-                                Ok(_) => tracing::debug!("heartbeat sent"),
-                                Err(err) => {
-                                    tracing::error!(%err, "failed to send heartbeat");
-                                }
-                            }
+                    // Heartbeat handler to send periodic messages.
+                    heartbeat_interval.tick().await;
+
+                    let msg: Message<String> = Message::default().event(models::phoenix::Event::Heartbeat);
+                    match client.send(msg).await {
+                        Ok(_) => tracing::debug!("heartbeat sent"),
+                        Err(err) => {
+                            tracing::error!(%err, "failed to send heartbeat");
+                            break; // automatic deconnection after a certain time.
                         }
                     }
                 }
             });
-        }
     }};
 }
